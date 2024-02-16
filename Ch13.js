c884a7095582a9d22fa3dd1d5aa8321c7d86ba00
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-02-16 17:11:57 -0500

    Chapter 13 notes

---
+++ Ch13.js
@@ -0,0 +1,15 @@
+/**
+ * HTTP protocol is used to get web pages hosted on the web
+ */
+/**
+ * HTML document format for web pages
+ */
+
+/**
+ * tags are inside <>
+ * for actual `<` character ==> use &lt;
+ * for `>` ==> &gt;
+ * for `&` ==> &amp;
+ * for `""` ==> &quot;
+ * rest of the stuff from the chapter is trivial
+ */
